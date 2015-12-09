69822afe0228c48bb8aadb8bbdd51ec3a2082911
rameshpanti <rameshpanti@gmail.com>
2015-12-09 18:44:54 +0530

    session-5-Assignment-2

--- fundamentals-assign-js/session-5-Assignment-1/circle-area.js
+++ fundamentals-assign-js/session-5-Assignment-1/circle-area.js
@@ -4,5 +4,5 @@ function calcCircleArea(r) {
 }
 
 document.getElementById("r1").innerHTML = "r = 7; area ="+""+ calcCircleArea(7);
-document.getElementById("r2").innerHTML = "r = 7; area ="+""+ calcCircleArea(1.5);
-document.getElementById("r3").innerHTML = "r = 7; area ="+""+ calcCircleArea(22);
+document.getElementById("r2").innerHTML = "r = 1.5; area ="+""+ calcCircleArea(1.5);
+document.getElementById("r3").innerHTML = "r = 22; area ="+""+ calcCircleArea(22);
---
+++ fundamentals-assign-js/session-5-Assignment-2/current-time.js
@@ -0,0 +1,4 @@
+var d = new Date();
+var time = d.getHours();
+var minut = d.getMinutes();
+document.write("Now the time is"+"="+time+""+":"+""+minut);
