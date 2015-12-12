5459c6170ef52c249eef8c16093e324de5816d6d
rameshpanti <rameshpanti@gmail.com>
2015-12-12 06:21:42 +0530

    Js Assignments

---
+++ fundamentals-assign-js/js-s6-assign-1-minMaxBumber/minmax.js
@@ -0,0 +1,8 @@
+var a = [2,4,56,34,28,12,9,100,1];
+var max = Math.max(...a);
+
+var min = Math.min(...a);
+
+document.getElementById("max").innerHTML = "Max number is = " + max;
+document.getElementById("min").innerHTML = "Min number is = " + min;
+
